5fdd0ae38ef83eef53958daa349a3d6861e0dde5
Bastian Gross <basti-gross@tu-dortmund.de>
2024-05-22 18:44:57 +0200

    Initial Commit

---
+++ Projektaufgabe_Diesel_vs_Elektroantrieb/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Projektaufgabe_Diesel_vs_Elektroantrieb
+//
+//  Created by Basti Gross on 22.05.24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!!!!!!\n";
+    return 0;
+}
